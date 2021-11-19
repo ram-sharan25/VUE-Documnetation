4574532ee8d6dd7ecb209a2caa40a58f99f1a746
Ram Sharan Rimal <ram-sharan25@github.com>
2021-11-20 00:37:37 +0545

    Added new single file component with props passing

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+    <hello-world /> 
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default{
+  components: { 
+    HelloWorld, 
+  },
+    name:"#app",
+    data(){
+        return{
+            message:"Hello World"
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="helloWorld">
+        <h1><span class="greeting">{{greeting}}</span><span class="greetingTo">{{msgTo}}</span></h1>
+        <button @click='changeGreeting("Hello")'>Say Hello</button>
+        <button @click='changeGreeting("Hi")'>Say Hi</button>
+        
+        
+    </div>
+</template>
+<script>
+export default{
+    name:"helloWorld",
+    data(){
+        return{
+            greeting :"Hello"
+
+        }
+    },
+    props:{
+        msgTo:{
+            type:String,
+            default:"World"
+        }
+    },
+    methods:{
+        changeGreeting(greeting){
+            this.greeting=greeting;
+
+        }
+
+    },
+
+}
+
+</script>
+<style scoped>
+</style>
