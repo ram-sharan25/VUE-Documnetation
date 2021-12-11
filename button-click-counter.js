3536225f7b47f633b662805365bcc4f3f1b44ffc
Ram Sharan Rimal <rimal.ram25@gmail.com>
2021-12-11 10:02:06 -0800

    Props to children not working

--- button-click-counter.js
+++ button-click-counter.js
@@ -1,20 +1,21 @@
 Vue.component('button-click-counter',{ 
-    template:'<button class=btn btn-primary @click="count++">{{count}}</button>',
-    props:['title']
-   
-    
-    
-});
-new Vue({
-    el:"#app",
+    template:`<button class=btn btn-primary @click="count++">{{count title}}</button>`,
+    props:['id','title'],
     data(){
         return{
             count:0,
             names:[
                 {id:1,title:'Mushu'},
                 {id:2,title:'Mayalu'},
-                {id:3,title:'Nakkali'}
+                // {id:3,title:'Nakkali'}
             ]
         }
     }
+   
+    
+    
+});
+new Vue({
+    el:"#app",
+    
 })
